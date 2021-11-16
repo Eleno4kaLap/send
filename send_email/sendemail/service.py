6f38e2054312d26b7c3784962db8ab2180a228d0
Spirlhark <Spirlhark@gmail.com>
2021-11-16 08:47:43 +0200

    first commit

---
+++ send_email/sendemail/service.py
@@ -0,0 +1,17 @@
+from django.core.mail import send_mail
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+def send(user_email):
+    print("THIS IS MY FUNCTION!!!!!!!!!!!!!!!!")
+    send_mail(
+        'Вы подписались на рассылку',
+        'Мы будем присылать Вам много спама',
+        'x.spirlhark.x@ukr.net',
+        [user_email],
+        fail_silently=False,
+    )
+    print("THIS IS MY FUNCTION!!!!!!!!!!!!!!!!")
+
